9339d8b36d481950f688935b5b2d490ab1a53e25
Nadia Santillán <nadiasantillan@gmail.com>
2026-05-13 08:51:41 -0300

    Modelo con SET1_ACT_AVG_BASE y SOL_ACT_AVG_BASE

--- pca.R
+++ pca.R
@@ -64,14 +64,14 @@ ggbiplot(pca, obs.scale = 1, var.scale = 1,
          ellipse = F, 
          circle = F) +
   theme_minimal() 
-
+str(melatonine)
 
 pca_for_model <- cbind(melatonine_nona,pca$x)
 pca_for_model$TratamientoDesc <- factor(pca_for_model$TratamientoDesc)
 
 library(car)
 fit_pc2_glmm <- glmmTMB(
-  PC2 ~ TratamientoDesc * StudyPeriodWeek + Work_status +  (1 | ParticipantID),
+  PC2 ~ TratamientoDesc * StudyPeriodWeek + Work_status + SOL_ACT_AVG_BASE + SET1_ACT_AVG_BASE+ (1 | ParticipantID),
   # Valor_Z ~ Metrica + Work_status + Treatment * Semana_Num + (1 | ParticipantID | Mes | estacion), hacer con sol_act y se_act
   # interacciones entre tratamiento y semana de tratamiento
   data = pca_for_model,
@@ -90,26 +90,19 @@ summary(fit_pc2_glmm)
 # summary(fit_pc1)
 
 fit_pc2 <- lmer(
-  PC2 ~ TratamientoDesc * StudyPeriodWeek + Work_status + (1 | ParticipantID ),
+  PC2 ~ TratamientoDesc * StudyPeriodWeek + Work_status+ SOL_ACT_AVG_BASE + SET1_ACT_AVG_BASE + (1 | ParticipantID ),
   data = pca_for_model,
 )
 summary(fit_pc2)
 
 fit_pc2_sin_estacion <- lmer(
-  PC2 ~ TratamientoDesc * StudyPeriodWeek + Work_status + (1 | ParticipantID ),
+  PC2 ~ TratamientoDesc * StudyPeriodWeek + Work_status+ SOL_ACT_AVG_BASE + SET1_ACT_AVG_BASE + (1 | ParticipantID ),
   data = pca_for_model,
 )
 summary(fit_pc2_sin_estacion)
 
-fit_pc3 <- lmer(
-  PC3 ~ TratamientoDesc * StudyPeriodWeek + Work_status + (1 | estacion/ParticipantID ),
-  data = pca_for_model
-)
-summary(fit_pc3)
-
-
 model_pc1 <- glmmTMB(
-  PC1 ~ Work_status + TratamientoDesc * StudyPeriodWeek + (1 | ParticipantID/estacion),
+  PC1 ~ Work_status + TratamientoDesc * StudyPeriodWeek + SOL_ACT_AVG_BASE + SET1_ACT_AVG_BASE+ (1 | ParticipantID/estacion),
   data = pca_for_model,
   family = gaussian()
 )
@@ -118,15 +111,15 @@ summary(model_pc1)
 
 library(sjPlot)
 x11(width = 12, height = 8)
-plot_model(model_pc1, 
+plot_model(fit_pc2_glmm, 
            type = "pred", 
-           terms = c("StudyPeriodWeek", "TratamientoDesc", "estacion", "Work_status"),
+           terms = c("StudyPeriodWeek", "TratamientoDesc", "SET1_ACT_AVG_BASE", "Work_status"),
            ci.lvl = 0.95) +
   # scale_color_manual(values = c("1" = "firebrick", "2" = "dodgerblue3"), 
   #                    labels = c("Placebo", "Melatonina 0.5mg")) +
   labs(title = "Predicciones del Modelo Mixto: Efecto del Tratamiento",
-       subtitle = "Controlado por Estacionalidad y Situación Laboral",
+       subtitle = "Controlado por Tratamiento, semana, eficiencia del sueño y situación laboral",
        x = "Semana de Estudio", 
-       y = "PC1",
+       y = "PC2",
        color = "Grupo") +
   theme_minimal()
